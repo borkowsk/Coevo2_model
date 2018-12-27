753f0c9afecc977ed580a331bf88fdb4c0277d42
Wojciech T. Borkowski <wborkowsk@gmail.com>
2018-12-27 21:47:12 +0100

    Wersja 2011 a

---
+++ src.old/src2011.06/ekologia.hpp.h
@@ -0,0 +1,20 @@
+// ekologia.hpp.h: interface for the ekologia class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_EKOLOGIA_HPP_H__5ED2EEC1_E452_4836_AAEC_BC300462CFB9__INCLUDED_)
+#define AFX_EKOLOGIA_HPP_H__5ED2EEC1_E452_4836_AAEC_BC300462CFB9__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class ekologia  
+{
+public:
+	ekologia();
+	virtual ~ekologia();
+
+};
+
+#endif // !defined(AFX_EKOLOGIA_HPP_H__5ED2EEC1_E452_4836_AAEC_BC300462CFB9__INCLUDED_)
