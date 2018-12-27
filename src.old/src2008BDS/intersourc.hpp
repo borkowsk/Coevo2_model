1af430d6a808f457939e2abfa5f8355e96c478de
Wojciech T. Borkowski <wborkowsk@gmail.com>
2018-12-27 21:19:01 +0100

    Wersja 2008 BDS

---
+++ src.old/src2008BDS/intersourc.hpp
@@ -0,0 +1,140 @@
+//
+//Zrodla do demonstracji interakcji drapieznik/ofiara za pomoca masek bitowych 
+/////////////////////////////////////////////////////////////////////////////////
+#ifndef __COEWO_INTERACTIONS_SOURCES_HPP_
+#define __COEWO_INTERACTIONS_SOURCES_HPP_
+
+#include "SYMSHELL/datasour.hpp"
+//Klasa udostepniajaca dowolna tablice prostokatna oraz jej wycinki.
+//Jesli zdefiniowany wycinek wykracza poza tablice zrodlowa to funkcja
+//get zwraca wartosc "miss" podawana w konstruktorze.Alternatywnie 
+//wycinek moze realizowac geometrie torusa i wtedy miss nie jest
+//potrzebne.
+
+class and_interaction_source:public rectangle_source_base
+//--------------------------------------------------------------
+{
+public:
+// Constructor 
+and_interaction_source( const char* itit):
+	rectangle_source_base(itit,256,256,1/*,subs,imiss*/){}
+
+~and_interaction_source()
+	{
+#ifndef NDEBUG
+	cerr<<"~and_interaction_source():"<<name()<<'\n';
+#endif
+	}
+
+void  bounds(size_t& num,double& min,double& max)
+//Ile elementow,wartosc minimalna i maksymalna
+	{
+	num=getrectgeometry()->get_width()*getrectgeometry()->get_height();
+	
+	if(ymin<ymax)//Sa dane
+		{
+		min=ymin;max=ymax;
+		return;
+		}
+
+	//Nie sa dane wiec probkujemy - co troche kosztuje	
+	min=0;
+	max=255;
+	}
+
+double get(iteratorh& p)
+//Daje nastepna z la*lb liczb!!! 
+	{
+	assert("get(iteratorh& p) NOT IMPLEMENTED!!!"==NULL);
+	//assert(p!=NULL);
+	if(p==NULL) return miss;
+	/*
+	size_t pom=_next( p );
+	if(pom!=ULONG_MAX)
+		return and(A,B);
+		else */
+		return miss;
+	}
+
+double get(size_t index)//Przetwarza index uzyskany z geometri
+	{ //na wartosc z serii, o ile jest mozliwe czytanie losowe	
+	assert(index<getrectgeometry()->get_size());
+	unsigned A=index/256;
+	unsigned B=index%256;
+	return and(A,B);
+	}	
+
+unsigned and(unsigned A,unsigned B)
+	{
+	return A&B;
+	}
+
+};
+
+
+class and_exploatation_source:public rectangle_source_base
+//--------------------------------------------------------------
+{
+public:
+// Constructor 
+and_exploatation_source( const char* itit):
+	rectangle_source_base(itit,256,256,1/*,subs,imiss*/){}
+
+~and_exploatation_source()
+	{
+#ifndef NDEBUG
+	cerr<<"~and_exploatation_source():"<<name()<<'\n';
+#endif
+	}
+
+void  bounds(size_t& num,double& min,double& max)
+//Ile elementow,wartosc minimalna i maksymalna
+	{
+	num=getrectgeometry()->get_width()*getrectgeometry()->get_height();
+	
+	if(ymin<ymax)//Sa dane
+		{
+		min=ymin;max=ymax;
+		return;
+		}
+
+	min=0;           //Brak dopasowania
+	max=and(255,255);//Pelne dopasowanie maski ataku do maski ochrony. 100% trawienia.
+	}
+
+double get(iteratorh& p)
+//Daje nastepna z la*lb liczb!!! 
+	{
+	assert("get(iteratorh& p) NOT IMPLEMENTED!!!"==NULL);
+	//assert(p!=NULL);
+	if(p==NULL) return miss;
+	/*
+	size_t pom=_next( p );
+	if(pom!=ULONG_MAX)
+		return and(A,B);
+		else */
+		return miss;
+	}
+
+double get(size_t index)//Przetwarza index uzyskany z geometri
+	{ //na wartosc z serii, o ile jest mozliwe czytanie losowe	
+	assert(index<getrectgeometry()->get_size());
+	unsigned A=index/256;
+	unsigned B=index%256;
+	return and(A,B);
+	}	
+
+double and(unsigned A,unsigned B)
+	{
+	//double(w.w.oslona & zabojca.w.w.geba)/(zabojca.w.w.geba)*
+	//		double(w.w.oslona & zabojca.w.w.geba)/(w.w.oslona)
+    if(A!=0 && B!=0)
+	    return double(A & B)/double(B) * double(A & B)/double(A);
+    else
+        return miss;
+	}
+
+};
+
+
+#endif
---
+++ src.old/src2008BDS/klonalinfo.cpp
@@ -0,0 +1,168 @@
+// klonalinfo.cpp: implementation of the klonalinfo class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include <assert.h>
+#include <math.h>
+#include "klonalinfo.hpp"
+
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+informacja_klonalna::informacja_klonalna(informacja_klonalna* iparent):
+    parent(NULL),
+    data_powstania(*zrodlo_makerow_czasu),
+    ostatnia_smierc(0),
+    zywych(0),
+    martwych(0),
+    wlasna_wartosc(0xFADEC0DE)
+{
+    assert(zrodlo_makerow_czasu!=NULL);
+    wlasna_wartosc=++licznik;//Zwiekszenie i zapamientanie aktualnej wartosci licznika klonow
+
+    parent=iparent;          //Zapamientanie klonu macierzystego/ojcowskiego
+    
+    if(parent!=NULL)//Jesli klon ma "ojca"
+        parent->childs(parent->childs.CurrSize())=this;//I rejestracja nowego klonu jako dziecka klonu macierzystego
+}
+
+informacja_klonalna::~informacja_klonalna() //Destruktor uzywany glownie dla zbyt malych
+{
+    usunieto++;
+    wlasna_wartosc=0xFADEC0DE;
+}
+      
+bool informacja_klonalna::OK(informacja_klonalna* who)
+//Sprawdza wartosc wlasna klonu - czy jest wlasciwa 
+{
+    if(who==NULL) 
+            return false;
+    if(who->wlasna_wartosc==0xFADEC0DE) 
+            return false;
+    if(who->wlasna_wartosc>licznik)
+            return false;
+    return true;
+}
+
+void informacja_klonalna::dolicz_indywiduum()
+                       //Zliczanie urodzin
+{
+    zywych++;
+}
+
+bool informacja_klonalna::odlicz_indywiduum()
+  //Zliczanie smierci
+{
+    zywych--;   assert(zywych<0xffffffff);
+    martwych++; 
+
+    if(zywych==0) //Ekstynkcja klonu
+    {
+        ostatnia_smierc=*zrodlo_makerow_czasu;
+        
+        if(tresh_kasowania>martwych //jesli klon bardzo maly
+            &&                      //to podejmujemy probe usuniecia
+           childs.CurrSize()==0     //o ile nie ma potomków 
+            &&
+           parent!=NULL             //i ma rodzica 
+            )
+        {                           
+            for(unsigned i=0;i<parent->childs.CurrSize();i++)
+                if(parent->childs[i]==this) //Szuka siebie na liscie dzieci swojego rodzica
+                {
+                    parent->childs.Del(i);
+                    return true;
+                }
+        }
+    }
+
+    return false;
+}
+
+unsigned long informacja_klonalna::czas_zycia_klonu()
+{
+    unsigned long end=ostatnia_smierc;
+    if(end==0) end=*zrodlo_makerow_czasu;
+    unsigned long ret=end-data_powstania;
+    return ret;
+}
+
+void informacja_klonalna::podlacz_marker_czasu(unsigned long* gdzie)
+//Mozna wywolac tylko raz na program!!!
+{
+    static int only_one_call=0;
+    assert(only_one_call==0);
+    only_one_call++;
+
+    //Ustalenie zrodla markerow czasu
+    zrodlo_makerow_czasu=gdzie;
+}
+
+void informacja_klonalna::ustaw_maksimum_kasowania(unsigned long ile)
+//Mozna wywolac tylko raz na program!!!
+{
+    static int only_one_call=0;
+    assert(only_one_call==0);
+    only_one_call++;
+
+    tresh_kasowania=ile;
+}
+
+bool informacja_klonalna::for_each_child(_akcja_trawersowania what_to_do,void* user_data)
+//Wykonanie akcji dla kazdego dziecka
+{
+ for(unsigned i=0;i<childs.CurrSize();i++)
+     if(childs[i]!=NULL)
+    {
+        bool ret=what_to_do(childs[i],user_data); 
+        if(!ret) return false;
+    }
+ return true;   
+}
+
+bool informacja_klonalna::trawersuj_drzewo_pre(_akcja_trawersowania what_to_do,void* user_data)
+//Trawersowanie z akcja wykonywana przed zajrzeniem do galezi
+{
+ bool ret=what_to_do(this,user_data);
+ 
+ if(!ret) return false;
+ 
+ for(unsigned i=0;i<childs.CurrSize();i++)
+     if(childs[i]!=NULL)
+    {
+        ret=childs[i]->trawersuj_drzewo_pre(what_to_do,user_data); 
+        if(!ret) return false;
+    }
+
+ return true;   
+}
+
+
+bool informacja_klonalna::trawersuj_drzewo_post(_akcja_trawersowania what_to_do,void* user_data)
+//Trawersowanie z akcja wykonywana po zajrzeniu do galezi
+{
+ for(unsigned i=0;i<childs.CurrSize();i++)
+   if(childs[i]!=NULL)
+    {
+        bool ret=childs[i]->trawersuj_drzewo_post(what_to_do,user_data); 
+        if(!ret) return false;
+    }
+
+ return what_to_do(this,user_data);
+}
+
+
+unsigned long informacja_klonalna::magnitude() 
+{
+	double pom=wszystkich();
+	if(pom==0) return 0;
+	pom=log(pom)/log(2);
+	return (unsigned long)(pom);
+} 	
+
+unsigned long informacja_klonalna::tresh_kasowania=0;
+unsigned long* informacja_klonalna::zrodlo_makerow_czasu=NULL;//Nie zainicjowana sensownie
+unsigned long informacja_klonalna::licznik=0; //Ile jest klonów
+unsigned long informacja_klonalna::usunieto=0;
