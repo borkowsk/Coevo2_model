81d23429377fea6f3c07a77241d959120495e961
Wojciech T. Borkowski <wborkowsk@gmail.com>
2018-12-27 21:31:10 +0100

    Wersja Styczen 2009

---
+++ src.old/src2009.01/co_agent.hpp
@@ -0,0 +1,232 @@
+#ifndef _COEWO_AGENT_HPP_
+#define _COEWO_AGENT_HPP_
+/* ROZMIAR TYPU BASE DECYDUJE O MOZLIWEJ KOMPLIKACJI SWIATA */
+/* JEST TYLE MOZLIWYCH TAXONOW ILE WZORCOW BITOWYCH W base2 */
+#include "INCLUDE/wb_bits.h"
+
+typedef unsigned char base;   // musi byc bez znaku
+typedef unsigned short base2; // musi miescic 2 zmienne base
+
+const unsigned BITS_PER_GENOM=16;   //!!! TO JEST ZAFIKSOWANE PRZEZ ROZMIAR base i base2
+const base2 MAXBASE2=(base2)0xffff;//ffffffffffffUL;
+const base  MAXBASE =(base)MAXBASE2;
+const base  AUTOTROF=MAXBASE;// wzor bitowy autotrofa - swiat go zywi
+
+//__declspec(align(8))  //TRZEBA ZAPEWNIÆ ¯EBY GENOM BY£ BITOW¥ CA£OŒCI¥
+#pragma pack(8)
+struct bity_wzoru
+{
+	base geba;	// bitowy wzorzec trybu odzywiania
+	base oslona;	// bitowy wzozec sposobu ochrony
+};
+
+union wzor
+{
+    base2 	_full; // pelny wzor bitowy "taxonu"
+    bity_wzoru w;  // i z podzialem na pola
+    //void clear(){_full=0;}
+};
+
+//WLASCIWA DEKLARACJA AGENTA
+///////////////////////////////////////////////////
+class agent//:public agent_base
+{    
+public:
+	class informacja_klonalna:public ::informacja_klonalna, public informacja_troficzna
+	{
+		wzor genom; //zeby bylo wiadomo co to za klon
+		unsigned  _specjalised;
+		unsigned _defensespec;
+		unsigned _feedingspec;
+	public:
+		informacja_klonalna(informacja_klonalna* parent,wzor wgenom):
+		  genom(wgenom),::informacja_klonalna(parent)
+		  {
+			  _specjalised=bits(genom.w.geba)+bits(genom.w.oslona);
+			  _defensespec=bits(genom.w.oslona);
+			  _feedingspec=bits(genom.w.geba);
+		  }
+
+		  unsigned long how_specialised() //Poziom specjalizacji mierzony w bitach
+		  {     return _specjalised;    }
+
+		  unsigned long how_specialised_in_defense() //Poziom specjalizacji mierzony w bitach
+		  {     return _defensespec;    }
+
+		  unsigned long how_specialised_in_feeding() //Poziom specjalizacji mierzony w bitach
+		  {     return _feedingspec;    }
+
+		  unsigned long feeding_niche()     //Jaka nisza troficzna
+		  {     return genom.w.geba; }
+
+		  unsigned long defense_niche()	//Jaka nisza obronna
+		  {     return genom.w.oslona; }
+
+		  //void dolicz_kontakt(unsigned long int marker_z_kim,double waga);
+		void dolicz_zezarcie(unsigned long int Identyfikator,double waga)
+		  {
+									assert(Identyfikator<=informacja_klonalna::ile_klonow());
+				dolicz_kontakt(Identyfikator,waga);
+		  }
+
+          void zapomnij_kontakty_recur(); //Zapomina kontakty ekologiczne u danego klonu i jego potomkow
+                    
+          //Konieczna reiplementacja metod klasy informacja_troficzna::
+          virtual
+              unsigned Waga()//Domyslna waga dla wezla (liczba osobnikow lub biomasa)
+          {
+              return this->ile_zywych();
+          }
+          virtual  
+              unsigned X()//X domyslnego polozenia wezla - np maska ataku
+          {
+              return genom.w.geba;
+          }
+          
+          virtual 
+              unsigned Y()//Y domyslnego polozenia wezla - np maska obrony
+          {
+              return genom.w.oslona;
+          }
+    };
+
+    wzor			 w; // wzor bitowy taxonu
+    
+    unsigned char wiek; // ile razy byl u sterowania, po przerolowaniu - smierc
+    unsigned	  sila; // zgromadzona energia
+    informacja_klonalna* klon;//Przynaleznosc klonalna i zwiazane z tym informacje
+  
+    /* Informacje statystyczne dotyczace indywiduow i klonów */
+    static unsigned max;//jaki jest najwiekszy taxon
+    static unsigned max_change;//i czy ostatnio max sie zmienil
+    static unsigned ile_ind;// ile jest zywych indywiduow
+    static unsigned ile_tax;// ile taxonow niezerowych
+    static unsigned ile_big_tax;// ile taxonow liczniejszych niz treshold
+    static unsigned liczniki[/*(size_t)MAXBASE2+1*/];// Liczniki liczebnosci aktualnie istniejacych 'taxonow ekologicznych" wg bitów wzoru
+     
+    //Wejscie/wyjscie
+    friend
+        ostream& operator << (ostream& o,const agent& a);
+    
+    friend
+        istream& operator >> (istream& i,agent& a);
+    
+    // rejestracja zmiany wartosci taxonu 
+    inline static   void tax(base2);  
+    
+    //Nie wirtualna metoda czyszczaca
+    void _clean()
+    {
+        if(klon!=NULL)
+        {
+            if(klon->odlicz_indywiduum())//Jesli nieistotny klon to trzeba usunac
+                delete klon;
+            klon=NULL;
+        }
+        w.w.geba=0; //Lepiej by bylo gdyby mozna ustawic -1
+        w.w.oslona=0;
+        wiek=sila=0;
+    }
+    
+    // TO CO MUSI byc zdefiniowane
+    ///////////////////////////////////
+    agent(const agent& ini):klon(NULL)
+    {
+        assert(&ini!=NULL);
+        
+        w._full=ini.w._full;wiek=ini.wiek;sila=ini.sila;
+        assert(ini.klon!=NULL);
+        klon->dolicz_indywiduum();
+    }
+    
+    agent():klon(NULL)
+    {
+        _clean();
+    }
+    
+    ~agent()
+    {
+        _clean();
+    }
+    
+    void clean()
+    {
+        if(jest_zywy())
+            kill();//Uzywa kill zeby wewnetrzne struktury statystyczne sie zgadzaly
+    }
+    
+    void assign_rgb(unsigned char Red,unsigned char Green,unsigned char Blue)
+    {assert("agent::assign_rgb(...) not implemented"==NULL);}
+    
+    //Sprawdzanie czy jest zywy
+    bool  jest_zywy()
+    { 
+        if(sila!=0 && wiek!=0) 
+        {
+            assert(klon!=NULL);
+            return true;
+        }
+        else
+            return false;
+    }
+  
+    bool  is_alive() { return jest_zywy();} //Bo wymaga tego klasa layer i pochodne
+    
+    unsigned long  how_old_taxon()
+    {
+       if(!jest_zywy()) 
+           return 0xffffffff;
+       else return klon->data_powstania_klonu();
+    }
+
+    int   how_specialised() //Poziom specjalizacji mierzony w bitach
+    {
+        if(!jest_zywy()) 
+            return -1;
+        else return klon->how_specialised();
+    }
+    
+    int   how_specialised_autotrof() //Poziom specjalizacji mierzony w bitach tylko dla autotrofów
+    {
+        if(!jest_zywy() || !(w.w.geba==0xff)) 
+            return -1;
+        else return klon->how_specialised();
+    }
+    
+    int   how_specialised_heterotrof() //Poziom specjalizacji mierzony w bitach tylko dla autotrofów
+    {
+        if(!jest_zywy() || (w.w.geba==0xff)) 
+            return -1;
+        else return klon->how_specialised();
+    }
+/*
+    int   how_specialised_in_defense() //Poziom specjalizacji maski obrony mierzony w bitach
+    {
+        if(!jest_zywy()) return -1;
+        else return klon->how_specialised_in_defense();
+    }
+
+    int   how_specialised_in_feeding() //Poziom specjalizacji maski ataku mierzony w bitach
+    {
+        if(!jest_zywy()) return -1;
+        else return klon->how_specialised_in_feeding();
+    }
+*/
+    //Podstawowa inicjacja i kasacja operujaca na licznikach taksonow
+    int init(base trof,base osl,unsigned isila); // inicjacja nowego agent
+    int kill();		     // smierc agenta - marker czasu dla statystyk
+    static base2 duplikuj(base2 r);  // kopiuje genotyp z mozliwa mutacja
+    
+    //Inicjacja i kasacja w interakcjach
+    int init(agent& rodzic); // inicjacja nowego jako potomka starego. Zwraca 0 jesli wadliwy genom
+    int kill(agent& zabojca,unsigned& energy_flow);// usmiercenie agenta przez drugiego
+    int parasit(agent& zabojca,unsigned& energy_flow);//zubazanie agenta przez drugiego. Zwraca 1 jesli zginal
+    
+    // oddzialywanie czasu
+    int uplyw_czasu();//Zwraca 1 jesli zginal. Marker czasu dla statystyk
+    
+    //Dla wygody implementacji:
+    friend class swiat;  
+};
+
+#endif
---
+++ src.old/src2009.01/klonalinfo.hpp
@@ -0,0 +1,90 @@
+// klonalinfo.hpp: interface for the klonalinfo class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_KLONALINFO_HPP__5A44210C_F5AD_4B72_9E75_E043CCEEAB52__INCLUDED_)
+#define AFX_KLONALINFO_HPP__5A44210C_F5AD_4B72_9E75_E043CCEEAB52__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "INCLUDE/arrays.hpp"
+using namespace ::wbrtm;
+
+class informacja_klonalna;
+typedef bool (*_akcja_trawersowania)(informacja_klonalna* current,
+									 void* user_data);//Akcja do wykonania na wezle drzewa taksonow
+																			//Jesli zwroci false to trawersowanie jest przerywane
+   
+//Przynaleznosc klonalna agenta i zwiazane z tym informacje
+class informacja_klonalna
+{   
+   static unsigned long     licznik; //Ile bylo klonów od poczatku - moze byc tylko powiekszany - zapewnia unikalne identyfikatory klonow
+   static unsigned long     usunieto;//Ile zostalo usunietych jako nieudane
+   static unsigned long*    zrodlo_makerow_czasu;//Referencja dostarczana przez uzytkownika klasy
+   static unsigned long     tresh_kasowania; //Przy jakim maksymalnym rozmiarze klonu wolno podjac probe jego kasowania 
+
+   informacja_klonalna* parent; //Wskaznik do klonu macierzystego
+   array_template<informacja_klonalna*> childs; //Wskazniki do klonów potomnych
+   
+   unsigned long data_powstania; //Kiedy powstal klon
+   unsigned long ostatnia_smierc; //"Data" ostatniej smierci
+   unsigned long zywych;         //Ile jest w tej chwili zywych
+   unsigned long martwych;       //Ile jest juz martwych od poczatku istnienia klonu
+   unsigned long wlasna_wartosc; //Wlasny numer klonu
+
+public:
+   //Skladowe statyczne 
+   static bool OK(informacja_klonalna* who);   //Sprawdza wartosc wlasna klonu - czy jest wlasciwa 
+   static void podlacz_marker_czasu(unsigned long* gdzie); //Mozna wywolac tylko raz na program!!!
+   static void ustaw_maksimum_kasowania(unsigned long ile);//Mozna wywolac tylko raz na program!!!
+   static unsigned long tresh_taksonow() { return tresh_kasowania;} //Jaki jest w tej symulacji treshold na rozmiar taksonu
+   static unsigned long ile_klonow() { return licznik; }
+   static unsigned long ile_taksonow() { return licznik-usunieto; } 
+
+   //Tworzenie/niszczenie 
+   informacja_klonalna(informacja_klonalna* parent);//Konstruktor	
+   virtual ~informacja_klonalna();             //Destruktor chyba niepotrzebny, ale jest na wszelki wypadek
+	  
+   //Skladowe indywidualne
+   virtual void dolicz_indywiduum();           //Zliczanie urodzin
+   virtual bool odlicz_indywiduum();           //Zliczanie smierci. Jesli "true" to trzeba/mozna usunac obiekt
+
+   unsigned long identyfikator(); 
+   unsigned long data_powstania_klonu(); 
+   unsigned long data_wymarcia_klonu();
+   unsigned long czas_zycia_klonu();
+   unsigned long ile_zywych();
+   unsigned long wszystkich();
+   unsigned long magnitude();
+   
+   bool for_each_child(_akcja_trawersowania what_to_do,void* user_data);//Wykonanie akcji dla kazdego dziecka
+   bool trawersuj_drzewo_pre(_akcja_trawersowania what_to_do,void* user_data);//Trawersowanie z akcja wykonywana przed zajrzeniem do galezi
+   bool trawersuj_drzewo_post(_akcja_trawersowania what_to_do,void* user_data);//Trawersowanie z akcja wykonywana po zajrzeniu do galezi
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Podreczna implementacja
+inline
+unsigned long informacja_klonalna::identyfikator()
+	{ return wlasna_wartosc;}
+
+inline 
+unsigned long informacja_klonalna::data_powstania_klonu() 
+	{return data_powstania;} 
+
+inline 
+unsigned long informacja_klonalna::data_wymarcia_klonu() 
+	{return ostatnia_smierc;} 
+
+inline 
+unsigned long informacja_klonalna::ile_zywych() 
+	{return zywych;} 
+
+inline 
+unsigned long informacja_klonalna::wszystkich() 
+	{return zywych+martwych;} 	
+
+#endif // !defined(AFX_KLONALINFO_HPP__5A44210C_F5AD_4B72_9E75_E043CCEEAB52__INCLUDED_)
