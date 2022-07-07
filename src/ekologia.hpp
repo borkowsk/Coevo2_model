f4076b28ae9b960d695843f51c40196e2efe1bea
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-07-07 14:35:56 +0200

    tmp.stage.202207071335

--- src/ekologia.hpp
+++ src/ekologia.hpp
@@ -29,7 +29,7 @@ class ekologia
             Identyfikator=Ident;//Unikalny identyfikator klonu - bo wskaznik do ori moze zostac uzyty powtornie
             X_lub_maska_ataku=X;
             Y_lub_maska_obrony=Y;
-            Zywych=Z;//Pomaga tez do wykrywania realokacji klonów (skasowanych)
+            Zywych=Z;//Pomaga tez do wykrywania realokacji klonÃ³w (skasowanych)
             co=ori;
         }
 
@@ -38,7 +38,7 @@ class ekologia
             Identyfikator=Ident;//Unikalny identyfikator klonu - bo wskaznik do ori moze zostac uzyty powtornie
             X_lub_maska_ataku=ori->X();
             Y_lub_maska_obrony=ori->Y();
-            Zywych=ori->Waga();//Pomaga tez do wykrywania realokacji klonów (skasowanych)
+            Zywych=ori->Waga();//Pomaga tez do wykrywania realokacji klonÃ³w (skasowanych)
             co=ori;
         }
     };
@@ -58,14 +58,14 @@ class ekologia
 private:
     unsigned user_tax_tres;  //Treshold uzytkownika - jesli inny niz z informacja_klonalna
 	double conn_treshold;    //Wizualizuje tylko te polaczenia, ktore stanowia 'gro' zasilajacej biomasy
-    agent::informacja_klonalna* ancestor; //Wskaznik do wspólnego przodka - zeby znajdowac zywe klony
-    array_template<node_info>  nodes;//Lista wez³ów sieci ekologicznej
+    agent::informacja_klonalna* ancestor; //Wskaznik do wspÃ³lnego przodka - zeby znajdowac zywe klony
+    array_template<node_info>  nodes;//Lista wezÅ‚Ã³w sieci ekologicznej
     array_template<connection> lines;//Lista lini rozpinajacych
 
     struct_array_source<node_info,unsigned int>* pNodeX;//Wspolrzedne wezlow w aranzacji
     struct_array_source<node_info,unsigned int>* pNodeY;//------------//----------------
     struct_array_source<node_info,unsigned int>* pNodeWeight;//Waga wezlow
-	struct_array_source<connection,size_t>* pConnPrey;//Indeksy pocz¹tków linii laczacych wezly sieci
+	struct_array_source<connection,size_t>* pConnPrey;//Indeksy poczÄ…tkÃ³w linii laczacych wezly sieci
 	struct_array_source<connection,size_t>* pConnPred;//Indeksy koncow linii laczacych wezly sieci
 	struct_array_source<connection,unsigned long>* pConnWeight;//Przeplyw w danej linii
 
@@ -77,15 +77,15 @@ private:
 	size_t _lowest_index_for_treshold(size_t node);
 public:
 	size_t HowManyNodes() { return nodes.CurrSize();}
-	double connection_tres(){ return conn_treshold;}  //Jaka granica wizualizacji po³¹czeñ
+	double connection_tres(){ return conn_treshold;}  //Jaka granica wizualizacji poÅ‚Ä…czeÅ„
 	unsigned tax_size_tres();  //Jak istotne dla ekosystemu taksony wizualizuje
-	void set_tax_size_tres(size_t T);//Ustawia granice wizualizacji - od nastêpnego updatu!
+	void set_tax_size_tres(size_t T);//Ustawia granice wizualizacji - od nastÄ™pnego updatu!
 
 	//Udostepnianie podstawowych serii danych do budowania sieci
 	linear_source_base* NodeX();//Polozenie wezla - np. maska ataku
 	linear_source_base* NodeY();//Polozenie wezla - np. maska obrony
-	linear_source_base* NodeWeight();//Waga wezla - np. liczba osobników (ewentualnie biomasa)
-	//linear_source_base* NodeScaledWeight();//Pierwiastkowana waga wezla - lepsze dla kó³ek
+	linear_source_base* NodeWeight();//Waga wezla - np. liczba osobnikÃ³w (ewentualnie biomasa)
+	//linear_source_base* NodeScaledWeight();//Pierwiastkowana waga wezla - lepsze dla kÃ³Å‚ek
 
 	//Informacje o polaczeniach
 	linear_source_base* ConnPrey();//Start polaczenia w ofierze (prey)
@@ -100,7 +100,7 @@ public:
 	
 	void operator = (agent::informacja_klonalna* TheAncestor);
 
-	virtual void aktualizuj_liste_wezlow(bool leftrec=true); //Aktualizacja listy wezlów
+	virtual void aktualizuj_liste_wezlow(bool leftrec=true); //Aktualizacja listy wezlÃ³w
 	virtual void zapomnij_liste(bool zwolnij_pamiec=false); //Zapominanie listy z ewentualnym zwalnianiem pamieci
 	size_t Znajdz(unsigned int Identyfikator);  //Znajduje wezel o podanym identyfikatorze i zwraca jego indeks na liscie
 
@@ -121,7 +121,7 @@ unsigned ekologia::tax_size_tres() //Wizualizuje tylko istotne dla ekosystemu ta
 
 inline
 void ekologia::set_tax_size_tres(size_t Tres)
-//Ustawia granice wizualizacji - od nastêpnego updatu!
+//Ustawia granice wizualizacji - od nastÄ™pnego updatu!
 {
 	 user_tax_tres=Tres;
 }
